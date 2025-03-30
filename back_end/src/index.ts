c29982e9e654570f687dd701a036e40c82ebfc85
Jamil Campeão <jamilcampeao@gmail.com>
2025-03-29 23:18:12 -0300

    Adicionado novas validações nas rotas e criado rotas de listagem de técnicos e cadastro de técnicos

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -22,8 +22,20 @@ app.get('/cidades', async (req: Request, res: Response) => {
     }
 });
 
+//Rota para cadastrar cidade
 app.post('/cidades', async (req: Request, res: Response) => {
     const { nome, estado, cep, cod_ibge } = req.body;
+
+    // Validações
+    if (!nome || !estado || !cod_ibge) {
+        res.status(400).json({ error: 'Nome, estado e código IBGE são obrigatórios' });
+        return;
+    }
+    if (cep && !/^\d{5}-\d{3}$/.test(cep)) {
+        res.status(400).json({ error: 'CEP inválido' });
+        return;
+    }
+
     try {
         const cidade = await prisma.cidade.create({
             data: {
@@ -40,7 +52,43 @@ app.post('/cidades', async (req: Request, res: Response) => {
         console.error('Erro ao criar cidade:', error);
         res.status(500).json({ error: 'Erro ao criar cidade'});
     }
-})
+});
+
+// Rota para listar técnicos
+app.get('/tecnicos', async (req: Request, res: Response) => {
+    try {
+        const tecnicos = await prisma.tecnico.findMany();
+        res.json(tecnicos);
+    }
+    catch (error) {
+        console.error('Erro ao buscar técnicos:', error);
+        res.status(500).json({ error: 'Erro ao buscar técnicos' });
+        
+    }
+});
+
+app.post('/tecnicos', async (req: Request, res: Response) => {
+    const { nome, situacao} = req.body;
+    // Validações
+    if (!nome || !situacao) {
+        res.status(400).json( { error: 'Nome e situação são obrigatórios' });
+        return;
+    }
+
+    try {
+        const tecnico = await prisma.tecnico.create({
+            data: {
+                TEC_NOME: nome,
+                TEC_SITUACAO: situacao
+            }
+        });
+
+    }
+    catch (error) {
+        console.error('Erro ao criar técnico:', error);
+        res.status(500).json({ error: 'Erro ao criar técnico'});
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
