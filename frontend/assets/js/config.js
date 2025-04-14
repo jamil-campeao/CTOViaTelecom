d1d959310eced48c710c37ff8f69650fe2a14945
Jamil Campeão <jamilcampeao@gmail.com>
2025-04-13 21:15:26 -0300

    Ajustado URL de produção

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://viatelecom.vercel.app/'; //URL producao (esta hospedado no vercel)
-export const API_URL = 'http://localhost:3000/';
+export const API_URL = 'https://viatelecom.vercel.app/'; //URL producao (esta hospedado no vercel)
+// export const API_URL = 'http://localhost:3000/';
